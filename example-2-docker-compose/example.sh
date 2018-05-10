eb388817b68390a003820d184cc9045f0cb2b9d4
Mladen Brndusic <brndusic@gmail.com>
2018-05-10 07:51:01 +0200

    fix: additiona curl request added to example

--- example-2-docker-compose/example.sh
+++ example-2-docker-compose/example.sh
@@ -3,5 +3,6 @@
 docker-compose up
 
 docker container run --network=private-example-network --rm appropriate/curl -fsSL http://static-file-server/
+docker container run --network=private-example-network --rm appropriate/curl -X HEAD -I http://static-file-server/
 
 docker-compose scale static-file-server-one=3 static-file-server-two=2
